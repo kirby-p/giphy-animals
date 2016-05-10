3a37b87c98f731a7f9c6cca792bf92b04ca826a5
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-05-10 11:35:11 -0400

    displays rating with gif

--- hw.js
+++ hw.js
@@ -1,7 +1,7 @@
 $('#addAnimal').on('click', function() {
     $('#gifs').empty();
     var newAnimal = $('#gifSearch').val().trim();
-    console.log(newAnimal);
+    // console.log(newAnimal);
 
     var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + newAnimal + '&api_key=dc6zaTOxFJmzC&limit=10';
 
@@ -10,14 +10,19 @@ $('#addAnimal').on('click', function() {
         method: 'GET'
     })
         .done(function(response){
-            console.log(response);
+            // console.log(response);
             var results = response.data;
 
             for(i = 0; i < results.length; i++){
+                var animalDiv = $('<div>');
                 var animalImage = $('<img>');
+                var p = $('<p>').text(("Rating: ") + results[i].rating);
+                // console.log(results[i].rating);
 
                 animalImage.attr('src', results[i].images.fixed_height.url);
-                $('#gifs').append(animalImage);
+                $(animalDiv).append(p);
+                $(animalDiv).append(animalImage);
+                $('#gifs').append(animalDiv);
 
             }
         });
