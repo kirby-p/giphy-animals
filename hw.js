a77d03c2c16920b6b076e9d42fead025933a67b5
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-05-11 09:39:06 -0400

    changed theme to music instruments, added preloaded buttons

--- hw.js
+++ hw.js
@@ -1,18 +1,28 @@
 $(document).ready(function(){
 
-    $('#addAnimal').on('click', function() {
-        var newAnimal = $('#gifSearch').val().trim();
-        var animalButton = $('<button>').text(newAnimal).attr('id', newAnimal).addClass('animalButton');
-        $('#buttons').append(animalButton);
+    var instrumentButtons = [
+        'guitar', 'bass', 'drums', 'trumpet', 'flute', 'clarinet', 'trombone', 'tuba', 'bassoon', 'violin', 'cello'
+    ];
+
+    for(i = 0; i < instrumentButtons.length; i++){
+        var instrumentButton = $('<button>').text(instrumentButtons[i]).attr('id', instrumentButtons[i]).addClass('instrumentButton');
+        $('#buttons').append(instrumentButton);
+
+    };
+
+    $('#addInstrument').on('click', function() {
+        var newInstrument = $('#gifSearch').val().trim();
+        var instrumentButton = $('<button>').text(newInstrument).attr('id', newInstrument).addClass('instrumentButton');
+        $('#buttons').append(instrumentButton);
         return false;
         });
 
-    $('#buttons').on('click', '.animalButton', function() {
+    $('#buttons').on('click', '.instrumentButton', function() {
         $('#gifs').empty();
-        var newAnimal = $(this).attr('id');
-        // console.log(newAnimal);
+        var newInstrument = $(this).attr('id');
+        // console.log(newInstrument);
 
-        var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + newAnimal + '&api_key=dc6zaTOxFJmzC&limit=10';
+        var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + newInstrument + '&api_key=dc6zaTOxFJmzC&limit=10';
 
         $.ajax({
             url: queryURL,
@@ -23,15 +33,19 @@ $(document).ready(function(){
                 var results = response.data;
 
                 for(i = 0; i < results.length; i++){
-                    var animalDiv = $('<div>');
-                    var animalImage = $('<img>');
+                    var instrumentDiv = $('<div>');
+                    var instrumentImage = $('<img>');
                     var p = $('<p>').text(("Rating: ") + results[i].rating);
                     // console.log(results[i].rating);
-                    console.log(i);
 
-                    animalImage.attr('src', results[i].images.fixed_height.url);
-                    $(animalDiv).append(p, animalImage).addClass('imageContainer');
-                    $('#gifs').append(animalDiv);
+                    // console.log(i);
+
+                    instrumentImage.attr({
+                        'src': results[i].images.fixed_height.url,
+                        'data-state': 'still'
+                    });
+                    $(instrumentDiv).append(p, instrumentImage).addClass('imageContainer');
+                    $('#gifs').append(instrumentDiv);
 
                 }
             });
